a0b3417185df311006ccd8ee891382f3fc18e7ab
Fady Raouf <100470798+fady231@users.noreply.github.com>
2022-12-09 18:05:48 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const student = require("./routes/students");
+const user = require("./routes/users");
+const FSE = require("./routes/FirstStadgeEn");
 
-//Importing route
-const questionsRoute = require('./routes/questions');
-const user = require('./routes/users');
-
-
-//Middlewares
 app.use(bodyParser.json());
-app.use('/questions', questionsRoute);
-app.use('/user', user);
+app.use(cors());
+app.use(express.static(path.join(__dirname, "/FSEimage")));
+app.use("/student", student);
+app.use("/user", user);
+app.use("/FSE", FSE);
 
 
 
-//ROUTES
-app.get('/', (req,res) => {
-    res.send('We are Home');
-});
-
-
-//Connecting to DB
 mongoose.connect(
-    "mongodb+srv://ahmed:7IxlE70oZXGK8WVz@cluster0.urdmbgm.mongodb.net/questions?retryWrites=true&w=majority", {
-        useNewUrlparser: true,
-        useUnifiedTopology: true,
-    },
-    () => console.log('connected to DB!'),
+  "mongodb+srv://ahmed:7IxlE70oZXGK8WVz@cluster0.urdmbgm.mongodb.net/questions?retryWrites=true&w=majority",
+  {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to DB!")
 );
 
 
-//How do we start listening to the server
- app.listen(3000)
+app.listen(3000);
